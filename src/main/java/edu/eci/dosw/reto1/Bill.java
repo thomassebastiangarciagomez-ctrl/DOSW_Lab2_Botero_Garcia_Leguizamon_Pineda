4ce974960af7f8642bf6abf7a5866eba5dd41172
Thomas Garcia <thomassebastiangarciagomez@gmail.com>
2026-08-03 10:00:02 -0500

    Creaton of Bill Class

---
+++ src/main/java/edu/eci/dosw/reto1/Bill.java
@@ -0,0 +1,15 @@
+package edu.eci.dosw.reto1;
+
+/**
+ * This class is for Laboratory 2 Challenge 1. This class is in construction
+ *
+ */
+public class Bill {
+    //List of products
+    //private int sumAll;
+    //Private Customer customer;
+
+    public Bill(){
+        //sumAll = 0;
+    }
+}
