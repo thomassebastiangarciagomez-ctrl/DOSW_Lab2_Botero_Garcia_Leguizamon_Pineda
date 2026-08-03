3a1035987c1511180da7d211187416f2108e097e
Thomas Garcia <thomassebastiangarciagomez@gmail.com>
2026-08-03 08:48:58 -0500

    chore Proyect Maven

---
+++ Laboratory2/dosw-lab-02/src/main/java/edu/eci/2w/App.java
@@ -0,0 +1,13 @@
+package edu.eci.2w;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Laboratory2/dosw-lab-02/src/test/java/edu/eci/2w/AppTest.java
@@ -0,0 +1,20 @@
+package edu.eci.2w;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
