aed9ed7a2d78051cb31adff8ac5e10165ea0abc7
JOSE DANIEL GARCIA PINEDA <jose.gpineda@mail.escuelaing.edu.co>
2026-08-10 07:59:55 -0500

    feat: implement hamburgerService and fix some parameters

--- src/main/java/edu/eci/dosw/reto2/HamburgerBuilder.java
+++ src/main/java/edu/eci/dosw/reto2/HamburgerBuilder.java
@@ -1,5 +1,44 @@
 package edu.eci.dosw.reto2;
+import java.util.List;
+import java.util.ArrayList;
 
+/**
+ * 
+ * HamburgerBuilder
+ */
 public class HamburgerBuilder {
-    
+    private List<Ingredient> ingredients;
+
+    public HamburgerBuilder(){
+        this.ingredients = new ArrayList<>();
+    }
+
+    /**
+     * Bread like hamburger ingredient
+     * @param price bread price
+     * @return hamburgerBuilder add bread
+     */
+    public HamburgerBuilder bread(double price){
+        ingredients.add(new Ingredient("bread", price));
+        return this;
+    }
+
+    /**
+     * Meat like hamburger ingredient
+     * @param price meat price
+     * @return hamburgerBuilder add meat
+     */
+    public HamburgerBuilder meat(double price){
+        ingredients.add(new Ingredient("meat", price));
+        return this;
+    }
+
+    /**
+     * Build hamburger
+     * @return hamburger with all ingredients
+     */
+    public Hamburger build(){
+        return new Hamburger(ingredients);
+    }
 }
+
--- src/main/java/edu/eci/dosw/reto2/HamburgerService.java
+++ src/main/java/edu/eci/dosw/reto2/HamburgerService.java
@@ -1,5 +1,13 @@
 package edu.eci.dosw.reto2;
 
 public class HamburgerService {
+
+    public static void main(String[] args){
+        Hamburger hamburger = new HamburgerBuilder()
+                                    .bread(10)
+                                    .meat(20)
+                                    .build();
+        System.out.println(hamburger.getIngredients().stream().mapToDouble(i -> i.getPrice()).sum());
+    }
     
 }
